4ab308fc1c3c4813530d5b167fdd99d19e85c210
landseer006 <77129667+landseer006@users.noreply.github.com>
2021-01-29 13:51:19 -0800

    Add js and css files

---
+++ movie.js
@@ -0,0 +1,5 @@
+/*In a separate JS file, movie.js, define a constructor function, Movie, which is used to define an object to hold the movie information.  The resulting objects should:
+•	Have a property to hold the movie title (this is set when the constructor is called)
+•	Have a property to hold the rating (this is set when the constructor is called)
+•	Have a method to validate if the two fields are valid. (the title field is not empty, and the rating is an integer between 1 and 5). It returns true or false, it does not use an Alert.
+•	Have a .toString() method that returns this objects title and rating as a single string*/
